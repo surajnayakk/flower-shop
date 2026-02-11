7524b7c24da7e4480003e5fb365ab54aeee4c4ad
surajnayakk <surajkumarnayak2204@gmail.com>
2026-02-11 23:09:12 +0530

    add accordian

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const menuHamburger = document.querySelector(".menu_hamburger");
 menuHamburger?.addEventListener("click", () => {
-    const nav = document.querySelector("nav");
-    nav?.classList.toggle("open");
-    menuHamburger.classList.toggle("open");
-})
+  const nav = document.querySelector("nav");
+  nav?.classList.toggle("open");
+  menuHamburger.classList.toggle("open");
+});
+
+const icons = document.querySelectorAll(".fa-plus");
+icons.forEach((icon) => {
+  icon?.addEventListener("click", () => {
+    const answer = icon.parentElement.nextElementSibling;
+    const isAnswerOpen = answer.classList.contains("open");
+    const answers = document.querySelectorAll(".answer");
+    answers?.forEach((answer) => {
+      answer.classList.remove("open");
+    })
+    if (!isAnswerOpen) {
+      answer?.classList.add("open");
+
+    }
+  });
+});
